5a4b8810c3c5d1a382209fa1dc6a1957e9911a43
Akhil <703320665@genpact.com>
2022-04-27 13:30:41 +0530

    BreakcontinueExample

---
+++ BreakcontinueExample.java
@@ -0,0 +1,12 @@
+public class BreakcontinueExample {
+
+    public static void main(String[] args) {
+            for(int i=0;i<=10;i++){
+                if(i==5){
+                    break;
+//           continue;
+                }
+                System.out.println("i = " + i);
+            }
+        }
+}
